a53727ffb4eebeff0356734d0c8308eda9835753
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2022-12-31 17:10:59 +0530

    added text reports

--- src/test/java/com/tests/base/TestSetup.java
+++ src/test/java/com/tests/base/TestSetup.java
@@ -2,9 +2,12 @@ package com.tests.base;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.appium.SelenideAppium;
+import com.codeborne.selenide.junit5.TextReportExtension;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(TextReportExtension.class)
 public class TestSetup {
 
   @BeforeEach
